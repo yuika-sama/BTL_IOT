82d76a1dba56e4aa29647c130f2cd7d21a1af4e7
Nguyen Duc Anh <nguyenanhduc2938@gmail.com>
2026-03-15 17:09:02 +0700

    Start from zero

--- be/src/middleware/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Async Handler Wrapper
- * Wraps async route handlers to catch errors and pass to error middleware
- */
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
